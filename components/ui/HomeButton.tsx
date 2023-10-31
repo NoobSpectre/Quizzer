1e0c2f50434953687e24a40bd457628ad7058263
soham <sohamdas1038@gmail.com>
2023-11-01 01:18:46 +0530

    added the home button to signin form and updated its position on the form

--- components/ui/HomeButton.tsx
+++ components/ui/HomeButton.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export const HomeButton = () => {
   return (
-    <div className="absolute bottom-4 w-full flex justify-end px-1 sm:px-3 md:px-6">
-      <Link href="/" className='hover:bg-gray-100 px-3 text-sm'>
+    <div className="absolute bottom-4 w-full flex justify-end px-4 sm:px-6 md:px-9">
+      <Link href="/" className='hover:bg-gray-200 pl-2.5 pr-[2px] text-sm'>
         <span className="bg-gradient-to-r from-red-600 to-indigo-600 bg-clip-text text-transparent font-semibold">
           &lt;&nbsp;&nbsp;Home
         </span>
