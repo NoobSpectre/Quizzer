4c36a91ef072f22dda3c3915b8e9a931cf3fa3f1
NoobSpectre <sohamdas1038@gmail.com>
2024-10-02 22:43:52 +0530

    add a new utility type WithArray

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,25 +1,37 @@
-import { AnyZodObject } from "zod";
-import { User } from "./api/users";
+import { AnyZodObject, ZodEffects } from "zod";
+import { User, UserWithId } from "./api/users";
 
-export type ErrorResponse = {
-  success: false;
-  message: string;
-  data: null;
-};
-
-export type SuccessResponse = {
-  success: true;
-  message: string;
-};
-
-export type SuccessResponseWithData<T> = SuccessResponse & {
-  data: T;
-};
-
-export type SuccessResponseWithUser = SuccessResponseWithData<User>;
+type WithArray<T> = T extends Array<infer U> ? U : T;
 
 export type RequestValidators = {
   params?: AnyZodObject;
-  body?: AnyZodObject;
+  body?: ZodEffects<AnyZodObject>;
   query?: AnyZodObject;
 };
+
+export type MessageResponse = {
+  message: string[];
+};
+
+export type ErrorResponse = MessageResponse & {
+  success: false;
+  data: null;
+};
+
+export type SuccessResponse<
+  T,
+  SensitiveKeys extends keyof WithArray<T> = never,
+> = MessageResponse & {
+  success: true;
+  data: T extends Array<infer U>
+    ? {
+        [key in keyof U as key extends SensitiveKeys ? never : key]: U[key];
+      }[]
+    : {
+        [key in keyof T as key extends SensitiveKeys ? never : key]: T[key];
+      };
+};
+
+export type SuccessResponseWithUser = SuccessResponse<User, "password">;
+export type SuccessResponseWithUserId = SuccessResponse<UserWithId, "password">;
+export type SuccessResponseWithUsers = SuccessResponse<User[], "password">;
