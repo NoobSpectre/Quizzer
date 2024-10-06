bf8044e24443d6574b18595341f54cdb3767a5eb
NoobSpectre <sohamdas1038@gmail.com>
2024-10-06 23:27:15 +0530

    configure the vite config with tanstack router plugin and alias abslute path from src to '@'

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [viteReact(), TanStackRouterVite()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
